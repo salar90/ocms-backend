bdf558e6c127fae28fc86311cb3675dceb9c2aaa
Salar Gholizadeh <salar.g@gmail.com>
2023-02-21 10:13:50 +0330

    fix route resource to api resource

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\EntryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,10 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->group(function(){
+Route::middleware('auth:sanctum')->group([], function(){
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
 
     
 });
+
+// Route::get('entries', [EntryController::class, 'index'])->name('entries.index');
+Route::apiResource('entries', EntryController::class);
