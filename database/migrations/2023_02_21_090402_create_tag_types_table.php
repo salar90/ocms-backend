63835048ccf0ee0228a611086014e1046bb38915
Salar Gholizadeh <salar.g@gmail.com>
2023-02-21 18:21:15 +0330

    add parents to tag type

--- database/migrations/2023_02_21_090402_create_tag_types_table.php
+++ database/migrations/2023_02_21_090402_create_tag_types_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('icon')->nullable();
             $table->string('status')->default('active');
+            $table->boolean('parents')->default('0');
             $table->timestamps();
         });
     }
